5e81e14258fe6a03af7ed88afc7f8a56031a1a25
nathanbkng <nathan.bokungu@student.uclouvain.be>
2025-05-13 19:02:34 +0200

    Collaborative strat : essaie d'amélioration sans succès

--- AntStrategy_collaborative.py
+++ AntStrategy_collaborative.py
@@ -1,4 +1,5 @@
 import random
+from typing import Optional
 
 import ant
 import environment
@@ -15,13 +16,17 @@ class CollabStrategy(AntStrategy):
     def __init__(self):
         # Track the last action to alternate between movement and pheromone deposit
         self.ants_last_action = {}  # ant_id -> (AntAction) last_action
-        self.ants_turns = {}  # ant_id -> (int) +1 if ant turned
+        self.ants_turns = {}  # ant_id -> (int) +1 if ant turned. initialize to 0 if MOVE_FORWARD
+        self.x = {}
+        self.y = {} ## essayer avec des dictionnaires ant_id -> (x,y) les coordonées globaux
 
 
     def decide_action(self, perception: AntPerception) -> AntAction:
 
         # Get ant's ID to track its actions
         ant_id = perception.ant_id
+        self.x[ant_id] = 0
+        self.y[ant_id] = 0
         last_action = self.ants_last_action.get(ant_id, None) # ne pas oublier de le mettre à jour !!!
 
         # Ant carries no food : search food
@@ -75,6 +80,7 @@ class CollabStrategy(AntStrategy):
                     if terrain == TerrainType.FOOD:
                         if dy > 0 and check_move(perception):  # Food is ahead in some direction
                             self.ants_turns[ant_id] = 0
+                            self.update_position(perception)
                             return AntAction.MOVE_FORWARD
 
                 best_dir = perception.get_food_direction()  # 0 p.ex. (NORTH)
@@ -93,6 +99,7 @@ class CollabStrategy(AntStrategy):
                     if terrain == TerrainType.COLONY:
                         if dy > 0 and check_move(perception):
                             self.ants_turns[ant_id] = 0
+                            self.update_position(perception)
                             return AntAction.MOVE_FORWARD
 
                 best_dir = perception.get_colony_direction()
@@ -112,8 +119,14 @@ class CollabStrategy(AntStrategy):
         ### FOURMIS restent bloquées à cause d'une équivalence de
 
         ant_dir = perception.direction.value
+        x_ant = self.x[perception.ant_id]
+        y_ant = self.y[perception.ant_id]
         max_level = 0
+        max_value = 0.0
+        best_dir = 0
         max_x, max_y = 0, 0
+
+
         for (x, y), ph_level in ph_type.items():
             if ph_level != None :
                 if ph_level > max_level:
@@ -124,20 +137,42 @@ class CollabStrategy(AntStrategy):
             # Random movement if no pheromone detected
             return self.decide_random_movement(perception)
 
+
+
         randomizer = random.random()  # pour empêcher de rester bloqué trop longtemps
 
-        ## S'active lorsqu'on atteint un certain stade
-        if perception.steps_taken > 150 and randomizer < 0.2:  #20% de chances de prendre une décision random
+        """
+        for direction in Direction:  # on itère sur toutes les cases voisines de la fourmi
+            dx, dy = Direction.get_delta(direction) # p.ex (1,0) pour East
+            value_sum = 0.0
+
+            for strength in range(1, 4):
+                check_x, check_y = x_ant + dx * strength, y_ant + dy * strength  # (1, 0), (2,0) et (3,0) pour East
+                if 0 <= check_x <= 35 and 0 <= check_y <= 35:
+                    ph_level = ph_type.get((check_x, check_y), 0.0)
+                    value_sum += ph_type.get((check_x, check_y), 0.0)
+
+
+            if value_sum > max_value:
+                max_value = value_sum
+                best_dir = direction
+        """
+
+        if max_value == 0 : # aucune phéromone captée
             return self.decide_random_movement(perception)
 
-        ## S'active lorsque trop de fourmis sont collées les unes aux autres
-        #if len(perception.nearby_ants) > 6 and randomizer < 0.5 and perception.steps_taken > 150:  # Ne fonctionne pas très bien
-            #return self.decide_random_movement(perception)
+        ## S'active lorsqu'on atteint un certain stade
+        if perception.steps_taken > 250 and randomizer < 0.15:  #15% de chances de prendre une décision random
+            return self.decide_random_movement(perception)
 
-        # Idée : sélectionner le deuxième meilleur mouvement au lieu de random
+        ## S'active lorsque trop de fourmis sont collées les unes aux autres # Ne fonctionne pas très bien
+        #if (len(perception.nearby_ants) > 10 and randomizer < 0.5
+                #and perception.steps_taken > 150 and perception.has_food):
+            #return self.decide_random_movement(perception)
 
         ph_dir = perception._get_direction_from_delta(max_x, max_y)
         return self.decide_turn(perception, ph_dir, ant_dir)
+        #return self.decide_turn(perception, best_dir, ant_dir)
 
     def decide_movement_old(self, perception: AntPerception) -> AntAction | None:
         """Decide which direction to move based on current state"""
@@ -260,15 +295,16 @@ class CollabStrategy(AntStrategy):
 
         # Random movement if no specific goal
         movement_choice = random.random()
-        pct = 0.70 # 70% chance to move forward
+        pct = 0.70 # 85% chance to move forward
         turn = 0.85 # 15% chance to turn left or right
 
-        if perception.steps_taken < 200 :
+        if perception.steps_taken < 100 : # autres pourcentages avant
             pct = 0.92
             turn = 0.96
 
         if check_move(perception) and movement_choice < pct :
                 self.ants_turns[ant_id] = 0
+                self.update_position(perception)
                 return AntAction.MOVE_FORWARD
         elif movement_choice < turn:
             if ant_id not in self.ants_turns:
@@ -290,6 +326,7 @@ class CollabStrategy(AntStrategy):
 
         if check_move(perception) and  (dirA == dirB):
                 self.ants_turns[ant_id] = 0
+                self.update_position(perception)
                 return AntAction.MOVE_FORWARD
         elif (dirA > dirB and dirA > 0) or (dirA < dirB and dirA == 0):
             if ant_id not in self.ants_turns:
@@ -334,3 +371,28 @@ class CollabStrategy(AntStrategy):
             if (-1, -1) in perception.visible_cells and perception.visible_cells[(-1, -1)] != TerrainType.WALL:
                 return True
         return False
+
+    def update_position(self, perception: AntPerception):
+        if perception.direction.value == 0:
+            self.y[perception.ant_id] -= 1
+        elif perception.direction.value == 1:
+            self.y[perception.ant_id] -= 1
+            self.x[perception.ant_id] += 1
+        elif perception.direction.value == 2:
+            self.x[perception.ant_id] += 1
+        elif perception.direction.value == 3:
+            self.x[perception.ant_id] += 1
+            self.y[perception.ant_id] += 1
+        elif perception.direction.value == 4:
+            self.y[perception.ant_id] += 1
+        elif perception.direction.value == 5:
+            self.x[perception.ant_id] -= 1
+            self.y[perception.ant_id] += 1
+        elif perception.direction.value == 6:
+            self.x[perception.ant_id] -= 1
+        elif perception.direction.value == 7:
+            self.x[perception.ant_id] -= 1
+            self.y[perception.ant_id] -= 1
+
+
+
